488a42d0a7a0b9e4ac83ef432bf9686bef497c1d
hackcheek <h4ckcheek@gmail.com>
2022-04-14 16:26:16 -0300

    Start

---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from parse import HelpPanel
+
+if __name__ == '__main__':
+    HelpPanel()
---
+++ parse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import sys
+from argparse import ArgumentParser
+from argparse import ArgumentError
+
+def ArgsControl(argv=None):
+
+    # Esta funcion analiza los parametros argumentos obtenidos
+
+    if not argv:
+        argv = sys.argv
+
+    parser = ArgumentParser(usage='python lfick.py [options] -u TARGET')
+
+    try:
+        parser.add_argument('-v', dest='verbose', help='Verbosity level: use -vv or more for greater effect')
+
+        # options = parser.add_argument('-u', '--url', dest='URL', type=str,
+                            # help='Target URL (Example: http://127.0.0.1/home/dashboard)')
+
+        # args = options.parse_args()
+
+    except (argparse.ArgumentError, TypeError) as ex:
+        parser.error(ex)
+
+    return argv
+
+def HelpPanel():
+    parser.print_help()
